6c0029717a3b942b752e76c45235b4cb9e77691c
Suvam Debnath <debnathsuvam322@gmail.com>
2025-09-18 00:20:26 +0530

    refactoring User Service to work in microservice architecture

---
+++ User/src/main/java/com/ecommerce/User/dto/AddressDTO.java
@@ -0,0 +1,12 @@
+package com.ecommerce.User.dto;
+
+import lombok.Data;
+
+@Data
+public class AddressDTO {
+    private String street;
+    private String city;
+    private String country;
+    private String zipcode;
+    private String state;
+}
---
+++ User/src/main/java/com/ecommerce/User/models/UserRole.java
@@ -0,0 +1,5 @@
+package com.ecommerce.User.models;
+
+public enum UserRole {
+    CUSTOMER, ADMIN
+}
