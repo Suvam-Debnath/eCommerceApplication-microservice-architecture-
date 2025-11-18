7c1a7882e566472f0abbca2179bf3883024902a4
Suvam Debnath <debnathsuvam322@gmail.com>
2025-11-18 21:17:30 +0530

    validated user via user microservice (used restclient with httpInterface for interservice  communication )[all microservice working fine]

--- order/src/main/java/com/ecommerce/order/clients/UserServiceClientConfig.java
+++ order/src/main/java/com/ecommerce/order/clients/UserServiceClientConfig.java
@@ -1,7 +1,5 @@
 package com.ecommerce.order.clients;
 
-import lombok.Builder;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatusCode;
@@ -12,20 +10,18 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import java.util.Optional;
 
 @Configuration
-public class ProductServiceClientConfig {
-
-
+public class UserServiceClientConfig {
     @Bean
-    public ProductServiceClient productServiceInterface(RestClient.Builder restClientBuilder){
+    public UserServiceClient userServiceClientInterface(RestClient.Builder restClientBuilder) {
         RestClient restClient = restClientBuilder
-                .baseUrl("http://product-service")
+                .baseUrl("http://user-service")
                 .defaultStatusHandler(HttpStatusCode::is4xxClientError,
                         ((request, response) -> Optional.empty()))
                 .build();
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory
-                                                .builderFor(adapter)
-                                                .build();
-        return factory.createClient(ProductServiceClient.class);
+                .builderFor(adapter)
+                .build();
+        return factory.createClient(UserServiceClient.class);
     }
 }
--- order/src/main/java/com/ecommerce/order/services/CartService.java
+++ order/src/main/java/com/ecommerce/order/services/CartService.java
@@ -1,8 +1,10 @@
 package com.ecommerce.order.services;
 
 import com.ecommerce.order.clients.ProductServiceClient;
+import com.ecommerce.order.clients.UserServiceClient;
 import com.ecommerce.order.dtos.CartItemRequest;
 import com.ecommerce.order.dtos.ProductResponse;
+import com.ecommerce.order.dtos.UserResponse;
 import com.ecommerce.order.models.CartItem;
 import com.ecommerce.order.repositories.CartItemRepository;
 import jakarta.transaction.Transactional;
@@ -21,16 +23,17 @@ public class CartService {
     private CartItemRepository cartItemRepository;
     @Autowired
     private ProductServiceClient productServiceClient;
+    @Autowired
+    private UserServiceClient userServiceClient;
 
     public boolean addToCart(String userId, CartItemRequest request) {
+        //look for product
         ProductResponse productResponse = productServiceClient.getProductDetails(request.getProductId());
         if (productResponse==null || productResponse.getStockQuantity()<request.getQuantity()) return false;
 
-//
-//        Optional<User> userOpt = userRepository.findById(Long.valueOf(userId));
-//        if (userOpt.isEmpty()) return false;
-//
-//        User user = userOpt.get();
+        UserResponse userResponse = userServiceClient.getUserDetails(userId);
+        if (userResponse == null)
+            return false;
 
         CartItem existingCartItem = cartItemRepository.findByUserIdAndProductId(userId, request.getProductId());
         if (existingCartItem != null){
