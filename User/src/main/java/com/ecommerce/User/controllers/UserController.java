0d669c1f2b69cffb83e4added77d9ede91a16029
Suvam Debnath <debnathsuvam322@gmail.com>
2025-11-20 20:50:52 +0530

    using SLF4J for structured Logging in user microservice

--- User/src/main/java/com/ecommerce/User/controllers/UserController.java
+++ User/src/main/java/com/ecommerce/User/controllers/UserController.java
@@ -3,6 +3,9 @@ package com.ecommerce.User.controllers;
 import com.ecommerce.User.dto.UserRequest;
 import com.ecommerce.User.dto.UserResponse;
 import com.ecommerce.User.services.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +15,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@Slf4j
 public class UserController {
 
     @Autowired
     private UserService userService;
+    //private static Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping
     public ResponseEntity<List<UserResponse>> getAllUsers(){
@@ -24,6 +29,14 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable String id){
+
+        log.info("Request received for user : {}", id);
+        log.trace("This is TRACE level - Very detailed logs");
+        log.debug("This is DEBUG level - Used for development debugging");
+        log.info("This is INFO level - General system information");
+        log.warn("This is WARN level - Something might be wrong");
+        log.error("This is ERROR level - Something failed");
+
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(()->ResponseEntity.notFound().build());
